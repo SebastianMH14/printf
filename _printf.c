16ffa74fb30b74ba69d78f38f817888a3330c41e
SebastianMH14 <sebasmh2002@hotmail.com>
2021-03-14 12:26:03 -0500

    _printf

---
+++ _printf.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+
+/**
+ * _printf - function that produces output according to a format
+ * @format: string to specify the format
+ * Return: the number of characters printed 
+ */
+int _printf(const char *format, ...)
+{
+    mod modif[] = {
+        {"%c", ...}
+        {"%s",....}
+        {"%d", ...}
+        {"%i", ...}
+        {NULL, NULL}
+    };
+}
